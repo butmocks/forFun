98a4a68a281ac75259b7b7b71db0346ff52f3f1b
butmocks <butmocks@gmail.com>
2022-08-31 05:12:31 +0300

    8

--- TruthOrSex/src/App.js
+++ TruthOrSex/src/App.js
@@ -32,7 +32,7 @@ class App extends React.Component {
                 <div className="card">
                     <h1 className="heading">{this.state.advice}</h1>
                     <button className="button" onClick={this.fetchAdvice}>
-                        <span>Give Me Advice</span>
+                        <span>Завдання</span>
                     </button>
                 </div>
             </div>
--- quote-generator-main/README.md
+++
@@ -1,7 +0,0 @@
-# Random Quote Generator App
-
-Live Link: [https://quote-generator-sage.vercel.app/](https://quote-generator-sage.vercel.app/)
-
-Here is the demo
-
-https://user-images.githubusercontent.com/75694208/143529674-2444b198-fcb9-4738-83b0-47bcab260276.mp4

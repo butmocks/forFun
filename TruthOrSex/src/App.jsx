45f84b2c8a15d9d44dfb1994d9afd3902a73e8f8
butmocks <butmocks@gmail.com>
2022-08-31 15:19:36 +0300

    13

--- TruthOrSex/src/App.jsx
+++ TruthOrSex/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+// mongodb+srv://Butmocks:zTGuBE01@cluster0.nyfyz7n.mongodb.net/test
 import './App.css';
 
 class App extends React.Component {
@@ -11,12 +11,12 @@ class App extends React.Component {
   }
   // https://data.mongodb-api.com/app/data-kdhdv/endpoint/data/v1
   // https://data.mongodb-api.com/app/data-kdhdv/endpoint
-
+  // https://europe-west1.gcp.data.mongodb-api.com/app/application-0-gzmgy/endpoint
   // https://realm.mongodb.com/api/client/v2.0/app/data-kdhdv/graphql
 
   fetchAdvice = () => {
     axios
-      .get('https://data.mongodb-api.com/app/data-kdhdv/endpoint')
+      .get('https://crudcrud.com/api/9f4758da4019400f9a974b9192052db1')
       .then(response => {
         const { advice } = response.data.slip;
 

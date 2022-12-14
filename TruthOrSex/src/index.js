4a02b015c7a7a07e3d6a809678b00ff4c9949ce0
Butmocks <butmocks@gmail.com>
2022-12-14 08:42:34 +0200

    gt

--- TruthOrSex/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/AllTasks";
-// import RandomTask from "./components/RandomTask"
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
